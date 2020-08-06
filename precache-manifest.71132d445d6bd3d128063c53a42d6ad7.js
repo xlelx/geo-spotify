77ea34e9376947f80978e5a00dca218b63a2128c
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-07 03:20:44 +1000

    Updates

---
+++ precache-manifest.71132d445d6bd3d128063c53a42d6ad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b10673518efc209db27cfe8fd4d16d6",
+    "url": "/geo-spotify/index.html"
+  },
+  {
+    "revision": "801a511ce3c836c3de38",
+    "url": "/geo-spotify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76682225c09d9b997331",
+    "url": "/geo-spotify/static/js/2.0595a8c6.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/geo-spotify/static/js/2.0595a8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801a511ce3c836c3de38",
+    "url": "/geo-spotify/static/js/main.5ce21f4d.chunk.js"
+  },
+  {
+    "revision": "de28cc386e3dc483fda8",
+    "url": "/geo-spotify/static/js/runtime-main.3df7ab3e.js"
+  },
+  {
+    "revision": "9fd6c7abdd33e7f2f2c9cd69fea75a76",
+    "url": "/geo-spotify/static/media/login.9fd6c7ab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.105781fd03cba22a6440eca4bae1c963.js"
+  "/geo-spotify/precache-manifest.71132d445d6bd3d128063c53a42d6ad7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/geo-spotify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
